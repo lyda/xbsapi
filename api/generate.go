c738f8807fa4da97aaff0a2e332b842406431f02
Kevin Lyda <kevin@lyda.ie>
2023-08-06 15:02:25 +0100

    Initial pass at an OpenAPI 3 API
    Using the swag generated swager 2 doc, converted it with the
    SwaggerHub editor and then made some basic changes to allow it
    to compile.
    
    Also used [yamlfix](https://lyz-code.github.io/yamlfix/) so
    that it will pass
    [yamllint](https://github.com/adrienverge/yamllint).  This will
    make it easier to highlight errors later.

---
+++ api/generate.go
@@ -0,0 +1,5 @@
+// Package api implements the API.
+package api
+
+//go:generate "$OAPI_CODEGEN" --config=types.cfg.yaml openapi.yaml
+//go:generate "$OAPI_CODEGEN" --config=server.cfg.yaml openapi.yaml
